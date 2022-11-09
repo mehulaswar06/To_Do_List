b06288031bfc9ace7d5151c65b9fa9f9e19d1b85
karthik <karthiksridhar19@siesgst.ac.in>
2022-11-09 22:35:11 +0530

    second commit

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const date =require(__dirname + "/date.js")
 
 
 
-const todo = express()
-todo.use(bodyParser.urlencoded({extended:true}))
-todo.use(express.static("public"))
+const todoo = express()
+todoo.use(bodyParser.urlencoded({extended:true}))
+todoo.use(express.static("public"))
 
-todo.set("view engine","ejs")
+todoo.set("view engine","ejs")
 
 
 
@@ -17,7 +17,7 @@ var items=["Buy food","Cook Food","Eat Food"];
 var workItems=[];
 
 
-todo.get("/",function(req,res){
+todoo.get("/",function(req,res){
     let day = date.getDay()
     // var dayNumber= today.getDay()
     
@@ -57,7 +57,7 @@ todo.get("/",function(req,res){
     // }
     res.render("list",{listTitle:day,newlistItems:items});
 })
-todo.post("/",function(req,res){
+todoo.post("/",function(req,res){
     console.log(req.body)
     let item=req.body.newitem;
 
@@ -73,16 +73,16 @@ todo.post("/",function(req,res){
 
 })
 
-todo.get("/work",function(req,res){
+todoo.get("/work",function(req,res){
     res.render("list",{listTitle:"Work",newlistItems: workItems});
 })
 
-todo.get("/about",function(req,res){
+todoo.get("/about",function(req,res){
     res.render("about")
 })
 
 
-todo.listen(3500, function(){
+todoo.listen(3500, function(){
     console.log("server is running at port 3500")
 })
 
